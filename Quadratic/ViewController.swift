e069c1b97dc649d345779f4b3da5eadeb7ca9e7f
dmattia <davidmattia@davids-mbp.dhcp.nd.edu>
2014-10-02 17:33:30 -0400

    Created a quadratic formula app.  Works with real numbers only.  Hoping to add non real functionality soon

--- Quadratic/ViewController.swift
+++ Quadratic/ViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var xx: UITextField!
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var x0: UITextField!
+    @IBOutlet weak var solution1: UILabel!
+    @IBOutlet weak var solution2: UILabel!
+    @IBOutlet weak var quad: UILabel!
+   
+    @IBAction func Solve(sender: AnyObject) {
+        var a = Double(xx.text.toInt()!)
+        var b = Double(x.text.toInt()!)
+        var c = Double(x0.text.toInt()!)
+        let det = b * b - 4 * a * c
+        let sqt = sqrt(det)
+        let sol1 = (-b + sqt) / (2 * a)
+        let sol2 = (-b - sqt) / (2 * a)
+        solution1.text = "\(sol1)"
+        solution2.text = "\(sol2)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
