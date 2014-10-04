d3d167b553004a3f534cba49089b96f5bbef5a75
dmattia <dmattia@nd.edu>
2014-10-04 13:34:25 -0400

    Updated to work with nonreal answers

--- Quadratic/ViewController.swift
+++ Quadratic/ViewController.swift
@@ -18,15 +18,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var quad: UILabel!
    
     @IBAction func Solve(sender: AnyObject) {
-        var a = Double(xx.text.toInt()!)
-        var b = Double(x.text.toInt()!)
-        var c = Double(x0.text.toInt()!)
+        var a:Double = Double(xx.text.toInt()!)
+        var b:Double = Double(x.text.toInt()!)
+        var c:Double = Double(x0.text.toInt()!)
         let det = b * b - 4 * a * c
-        let sqt = sqrt(det)
-        let sol1 = (-b + sqt) / (2 * a)
-        let sol2 = (-b - sqt) / (2 * a)
-        solution1.text = "\(sol1)"
-        solution2.text = "\(sol2)"
+        if (det >= 0) {
+            let sqt = sqrt(det)
+            let sol1 = (-b + sqt) / (2 * a)
+            let sol2 = (-b - sqt) / (2 * a)
+            solution1.text = "\(sol1)"
+            solution2.text = "\(sol2)"
+        } else {
+            let d = -det;
+            let sqt = sqrt(d)
+            let realPart = -b / (2*a)
+            let imagPart = abs(sqt / (2*a))
+            solution1.text = "\(realPart) + \(imagPart)i"
+            solution2.text = "\(realPart) - \(imagPart)i"
+           
+        }
     }
     
 
