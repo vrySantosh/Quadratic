564d5093021b75e0c1663b44bdab830af2a889c8
dmattia <dmattia@nd.edu>
2014-10-09 23:26:16 -0400

    Added a home screen

---
+++ Quadratic/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  Quadratic
+//
+//  Created by David Mattia on 10/9/14.
+//  Copyright (c) 2014 David Mattia. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToHome(segue : UIStoryboardSegue) {}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
